005ddd4ba0c736bde81905820cb7d50e8e7d4e64
spectrumm12 <134535415+spectrumm12@users.noreply.github.com>
2023-06-27 12:29:45 +0300

    Add files via upload

---
+++ work.py
@@ -0,0 +1,7 @@
+def sum(a,b):
+    return(a+b)
+    c=int(input("ЗДОРОВО ЧУВАК НАПИШИ ЛЮБЫЕ данные"))
+    d=int(input)("введи число")
+    print(sum(d,c),"сумма")
+    print(привет)
+    print(покаааа)
